e7b1f75e1e1d282ed1272a61efefdfb58ca2288e
ASUS Vivobook <ASUS Vivobook@LAPTOP-RA51567G>
2024-09-29 22:54:20 +0700

    Add project files.

---
+++ THWebCB/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace THWebCB.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Bai2()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Bai2ab()
+        {
+            ViewBag.Message = "Gộp hàng và cột.";
+
+            return View();
+        }
+        public ActionResult Bai3()
+        {
+            ViewBag.Message = "Xây dựng bảng.";
+
+            return View();
+        }
+    }
+}
---
+++ THWebCB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="THWebCB.MvcApplication" Language="C#" %>
